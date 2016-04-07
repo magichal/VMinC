dadfa87403b1358be4ffd2ed70884b11b10e0070
neaugu <guillaume.neau@ensta-bretagne.org>
2016-04-07 13:17:11 +0200

    Inter_final

--- src/Inter_ASS/interpret.c
+++ src/Inter_ASS/interpret.c
@@ -1,18 +1,5 @@
 #include "interpret.h"
 
-void affichebin(unsigned n)
-{
-	unsigned bit = 0 ;
-	unsigned mask = 1 ;
-	int i;
-	for (i=0 ; i < 32 ; ++i)
-	{
-		bit = (n & mask) >> i ;
-		printf("%d", bit) ;
-		mask <<= 1 ;
-	}
-}
-
 char * bin (unsigned long int i)
 {
     static char buffer [1+sizeof (unsigned long int)*8] = { 0 };
@@ -23,33 +10,35 @@ char * bin (unsigned long int i)
 
 
 
-int main (int argc, char * argv[]){ 
+void createCode (const char *filepath,unsigned Commande[1000]){ 
 	
 	
 	Tableau *tab = malloc(sizeof(Tableau));
 	Tableau *courant = malloc(sizeof(Tableau));
-	unsigned Commande[1000];
 	
 		
-	AnalyseText("load.txt", tab);//&Tableau);
+	AnalyseText(filepath, tab);
 	int i=0;
 	courant=tab;
 	
-	while((courant->suivant)!=NULL){
-	printf("%s\n",courant->mot);
-	courant=courant->suivant;
-	i++;
-	}
-	
 	AnalyseLexique(tab,Commande);
 	int j=0;
-	while(Commande[j]!=0){
-	printf("%s\n",bin(Commande[j]));
+	
+	
+	while(Commande[j]!=0x98000000){
+	printf("Binaire : %s\n",bin(Commande[j]));
 	j++;
 	}
-	printf("\n");
-	printf("%08X\n",Commande[0]);
+	printf("Binaire : %s\n",bin(Commande[j]));
 	
-	
-	return 0;
 }
+/*
+void main(){
+
+	unsigned Commande[10000];
+	createCode("braz.txt",Commande);
+	return;
+
+
+}
+*/
